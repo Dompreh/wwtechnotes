b2fd83cc43da089681b1e047eed8b2b62c6a541b
Dompreh <stephenasiedu3654@gmail.com>
2023-01-19 19:16:31 +0000

    entire-bug

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { store } from './app/store';
 import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
 
-if(process.env.NODE_ENV === 'production') disableReactDevTools
+if(process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
